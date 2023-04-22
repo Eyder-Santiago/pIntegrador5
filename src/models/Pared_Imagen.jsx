ddc39b3b44a954a7fdb58761498985947be824af
Eyder Suarez <eyder.suarez@correounivalle.edu.co>
2023-04-21 23:45:45 -0500

    trabajo completo

---
+++ src/models/Pared_Imagen.jsx
@@ -0,0 +1,32 @@
+import React, {useState, useRef } from 'react';
+import {Plane, useTexture} from '@react-three/drei';
+export function Pared_Imagen() {
+  const paredRef = useRef(null)
+
+  const images = ["/imagenes/gato.jpg", "/imagenes/oveja.jpg", "/imagenes/leon.jpg"];
+  const [imagenPared, setImagenPared] = useState(0);
+  const textureImagenes = useTexture(images[imagenPared]);
+  
+
+  function onPareIMG() {
+    setImagenPared((imagenPared + 1) % images.length);
+  }
+
+
+   return (
+      <group ref={paredRef} onClick={onPareIMG}>
+      <Plane 
+        
+        args={[5, 3]}  
+        rotation={[Math.PI / 1, 0, 3.1]}
+        position={[0, 1.5, 2]} 
+        color="#000000" 
+        
+      >
+        <meshStandardMaterial map={textureImagenes}/>
+      </Plane>
+      </group>
+    );
+   
+}
+export default Pared_Imagen;
---
+++ src/models/Pared_Video.jsx
@@ -0,0 +1,34 @@
+import React, {useRef, useEffect  } from 'react';
+import { Plane } from '@react-three/drei';
+import { useVideoTexture } from '@react-three/drei'
+
+export function Pared_Video() {
+
+  const texture1 = useVideoTexture("Video/musica.mp4")
+  const paredRef = useRef(null)
+  function onPareMOD() {
+   
+  }
+  useEffect(() => {
+    const video = paredRef.current.children[0].material.map.image;
+    video.pause(); 
+  }, []);
+
+  function onPareMOD() {
+    const video = paredRef.current.children[0].material.map.image;
+    if (video.paused) {
+      video.play(); 
+    } else {
+      video.pause(); 
+    }
+  }
+    return (
+      <group  ref={paredRef} onClick={onPareMOD}>
+        <Plane args={[5, 3]}  rotation={[-Math.PI / 50.9, 0, 0]}position={[0, 1.5, -2]} color="#fafafa" >
+          <meshBasicMaterial map={texture1}  />
+       </Plane>
+        
+      </group>
+      );
+}
+export default Pared_Video;
